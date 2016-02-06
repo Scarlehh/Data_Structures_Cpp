f03be5dfe086be67af63ee0ab755193aaf6d8e1e
Scarlett <gourleys@tcd.ie>
2016-02-06 19:46:06 +0000

    Arraylist main to test compilation and working output function

---
+++ ArrayList/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 6 Feb 2016
+ *      Author: scarlett
+ */
+
+#include <iostream>
+#include "ArrayList.h"
+
+int main() {
+	ArrayList<int> list;
+	std::cout << list << "\n";
+
+	return 0;
+}
